9571866fa2fd155296b70d24aa23d7ef150c3f46
Anthony Gutierrez <crisgutierrezp12@gmail.com>
2026-03-25 22:34:11 -0600

    comienzo del proyecto

---
+++ source/estructuras/listas/normal/Nodo.h
@@ -0,0 +1,14 @@
+#ifndef NODO_H
+#define NODO_H
+
+#include "source/pojo/Product.h"
+
+class Nodo {
+    private:
+        Nodo *anterior;
+        Nodo *siguiente;
+        Product* dato;
+    public:
+        
+};
+#endif
---
+++ source/pojo/Product.h
@@ -0,0 +1,15 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include <String>
+
+using namespace std;
+
+class Product {
+    private:
+        
+    public:
+
+};
+
+#endif
